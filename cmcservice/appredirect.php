9cb268f1b7dbf8a1b369ef904a5607c2fd0b0325
Mukul Kumar <mukul.05ind@gmail.com>
2016-01-20 14:28:13 +0530

    minor change

--- cmcservice/appredirect.php
+++ cmcservice/appredirect.php
@@ -1,12 +1,10 @@
 <?php
-
 $iPod    = stripos($_SERVER['HTTP_USER_AGENT'],"iPod");
 $iPhone  = stripos($_SERVER['HTTP_USER_AGENT'],"iPhone");
-$Android = stripos($_SERVER['HTTP_USER_AGENT'],"Android");
+$Android = stripos($_SERVER['HTTP_USER_AGENT'],"mobile");
 
 if( $iPod || $iPhone ){
-    echo 'Iphone app comming soon!';die;
-    //header('Location: https://play.google.com/store/apps/details?id=com.clubmycab');
+    header('Location: https://itunes.apple.com/in/app/ishareryde/id1073560784?ls=1&mt=8');
 }else if($Android){
     header('Location: https://play.google.com/store/apps/details?id=com.clubmycab');
 }
