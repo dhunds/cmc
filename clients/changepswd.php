9ba1e653947130f0b5ae704ad98b1229989cf7b8
Mukul Kumar <mukul.05ind@gmail.com>
2016-01-28 16:27:40 +0530

    removed dropped rides from myriad api, change password added for client panel

--- clients/changepswd.php
+++ clients/changepswd.php
@@ -2,6 +2,28 @@
 include_once('connection.php');
 include_once('header.php');
 include_once('topmenu.php');
+
+$msg = '';
+if (isset($_POST['submitfrm'])){
+    $sql = "SELECT id FROM clients WHERE password='".$_POST['currentpass']."' AND id=".$_SESSION['userId'];
+    $stmt = $con->prepare($sql);
+    $stmt->execute();
+    $rowCount = (int) $stmt->rowCount();
+
+    if ($rowCount > 0){
+        $sql = "UPDATE clients SET password='".$_POST['newpswd']."' WHERE id=".$_SESSION['userId'];
+        $stmt = $con->prepare($sql);
+
+        if ($stmt->execute()){
+            $msg = 'Password changed successfully!';
+        } else{
+            $msg = 'An error occured. Please try again.';
+        }
+    } else{
+        $msg = 'Invalid current password.';
+    }
+}
+
 ?>
 
 <div class="content pure-u-1-1 pure-u-md-3-4">
@@ -9,24 +31,25 @@ include_once('topmenu.php');
 
         <div>
             <h4 class="headingText">Change Password</h4>
+            <p style="margin-left: 10px;" id="errormsg"><?=$msg;?></p>
             <div style="padding: 15px;">
-                <form method="post" action="" enctype="multipart/form-data">
+                <form method="post" action="" onsubmit="return changepswd();">
                     <div>
-                        <div class="divRight bluetext"><input type="text" name="currentpass" id="currentpass" placeholder="Current Password"></div>
+                        <div class="divRight bluetext"><input type="password" name="currentpass" id="currentpass" placeholder="Current Password"></div>
                         <div style="clear:both;"></div>
                         <br/>
 
-                        <div class="divRight bluetext"><input type="text" name="newpswd" id="newpswd" placeholder="New Password">
+                        <div class="divRight bluetext"><input type="password" name="newpswd" id="newpswd" placeholder="New Password">
                         </div>
                         <div style="clear:both;"></div>
                         <br/>
 
-                        <div class="divRight bluetext"><input type="text" name="confirmpswd" id="confirmpswd" placeholder="Repeat Password">
+                        <div class="divRight bluetext"><input type="password" name="confirmpswd" id="confirmpswd" placeholder="Repeat Password">
                         </div>
                         <div style="clear:both;"></div>
                         <br/>
 
-                        <div class="divLeft bluetext"><input type="submit" name="submit" value="Change Password" class="cBtn">
+                        <div class="divLeft bluetext"><input type="submit" name="submitfrm" value="Change Password" class="cBtn">
                         </div>
                         <div class="divRight bluetext"></div>
                     </div>
@@ -41,5 +64,33 @@ include_once('topmenu.php');
 </div>
 
 <?php
-include_once('footer.php');
+    include_once('footer.php');
 ?>
+
+<script>
+
+    function changepswd(){
+
+        if ($( "#currentpass" ).val() ==''){
+            $( "#errormsg").text('Please enter current password');
+            return false;
+        }
+
+        if ($( "#newpswd" ).val() ==''){
+            $( "#errormsg").html('Please enter new password');
+            return false;
+        }
+
+        if ($( "#newpswd" ).val().length < 6){
+            $( "#errormsg").html('New password too short.');
+            return false;
+        }
+
+        if ($( "#newpswd" ).val() != $( "#confirmpswd" ).val()){
+            $( "#errormsg").html('New password and confirm password does not match.');
+            return false;
+        }
+        return true;
+    }
+
+</script>
--- clients/dashboard.php
+++ clients/dashboard.php
@@ -2,6 +2,26 @@
 include_once('connection.php');
 include_once('header.php');
 include_once('topmenu.php');
+
+$sql = "SELECT PoolId FROM userpoolsmaster JOIN clientGroups ON clientGroups.groupId=userpoolsmaster.PoolId AND clientGroups.clientId=".$_SESSION['userId'];
+
+$stmt = $con->prepare($sql);
+$stmt->execute();
+$groupCount = (int) $stmt->rowCount();
+$groups = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$groupids='';
+
+foreach ($groups as $val) {
+    $groupids .=$val['PoolId'].',';
+}
+$groupids = substr($groupids,0,-1);
+
+$sql = "SELECT PoolSubId FROM userpoolsslave WHERE PoolId IN ($groupids)";
+$stmt = $con->prepare($sql);
+$stmt->execute();
+$groupMemberCount = (int) $stmt->rowCount();
+
 ?>
 
 <div class="content pure-u-1-1 pure-u-md-3-4">
@@ -10,15 +30,11 @@ include_once('topmenu.php');
             <h4 class="headingText">Account Summary: Nagarro</h4>
             <div class="pure-g pure-g1 dashboard-summary-heading">
                 <div class="pure-u-6-24"><p class="dashboard-summary-title">Total Groups</p></div>
-                <div class="pure-u-18-24"><p>25</p></div>
+                <div class="pure-u-18-24"><p><?=$groupCount;?></p></div>
             </div>
             <div class="pure-g pure-g1 dashboard-summary-heading">
                 <div class="pure-u-6-24"><p class="dashboard-summary-title">Total Members</p></div>
-                <div class="pure-u-18-24"><p>25</p></div>
-            </div>
-            <div class="pure-g pure-g1 dashboard-summary-heading">
-                <div class="pure-u-6-24"><p class="dashboard-summary-title">Total Rides</p></div>
-                <div class="pure-u-18-24"><p>25</p></div>
+                <div class="pure-u-18-24"><p><?=$groupMemberCount;?></p></div>
             </div>
             <div style="clear:both;"></div>
         </div>
