1550c92300b21bc09a584052c38159eaa0a3bcf3
Mukul Kumar <mukul.05ind@gmail.com>
2016-02-03 11:32:55 +0530

    Dummy login for apple testing

--- cmcservice/login.php
+++ cmcservice/login.php
@@ -5,35 +5,43 @@ $objNotification = new Notification();
 
 $MobileNumber = $_POST['MobileNumber'];
 
-$stmt = $con->query("SELECT * FROM registeredusers WHERE Trim(MobileNumber) = Trim('$MobileNumber')");
-$rows = $con->query("SELECT FOUND_ROWS()")->fetchColumn();
-
-if ($rows > 0) {
+if ($MobileNumber == '00919810000000') {  // Dummy login for apple testing
+    $stmt = $con->query("SELECT * FROM registeredusers WHERE Trim(MobileNumber) = Trim('$MobileNumber')");
+    $rows = $con->query("SELECT FOUND_ROWS()")->fetchColumn();
     $usrRows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    $singleusepassword = rand(100000, 999999);
-    $timestamp = date('Y-m-d H:i:s', strtotime('+1 day'));
-
-    $stmt = $con->query("SELECT SmsMessage FROM smstemplates WHERE SmsshortCode = 'OTP'");
-    $message = $stmt->fetchColumn();
-    $message = str_replace("XXXXXX", $singleusepassword, $message);
-    $MobileNumber = '[' . $MobileNumber . ']';
-
-    $objNotification->sendSMS($MobileNumber, $message);
-
-    /*if ($usrRows[0]['Email'] != '') {
-        $emailBody = 'Hi ' . $usrRows[0]['FullName'] . ',<br/><br/>' . $message . '<br/><br/>Thanks,<br/>iShareRyde Team';
-        $params = array('to' => $usrRows[0]['Email'], 'body' => $emailBody);
-        $resp = $objNotification->sendEmailOTP($params);
-    }*/
-
-    $MobileNumber = $_POST['MobileNumber'];
-
-    $sql2 = "UPDATE registeredusers SET SingleUsePassword='$singleusepassword', SingleUseExpiry= '$timestamp' WHERE Trim(MobileNumber) = Trim('$MobileNumber')";
-    $stmt2 = $con->prepare($sql2);
-    $res2 = $stmt2->execute();
     echo json_encode($usrRows);
+    exit;
 } else {
-    echo "login error";
+    $stmt = $con->query("SELECT * FROM registeredusers WHERE Trim(MobileNumber) = Trim('$MobileNumber')");
+    $rows = $con->query("SELECT FOUND_ROWS()")->fetchColumn();
+
+    if ($rows > 0) {
+        $usrRows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $singleusepassword = rand(100000, 999999);
+        $timestamp = date('Y-m-d H:i:s', strtotime('+1 day'));
+
+        $stmt = $con->query("SELECT SmsMessage FROM smstemplates WHERE SmsshortCode = 'OTP'");
+        $message = $stmt->fetchColumn();
+        $message = str_replace("XXXXXX", $singleusepassword, $message);
+        $MobileNumber = '[' . $MobileNumber . ']';
+
+        $objNotification->sendSMS($MobileNumber, $message);
+
+        /*if ($usrRows[0]['Email'] != '') {
+            $emailBody = 'Hi ' . $usrRows[0]['FullName'] . ',<br/><br/>' . $message . '<br/><br/>Thanks,<br/>iShareRyde Team';
+            $params = array('to' => $usrRows[0]['Email'], 'body' => $emailBody);
+            $resp = $objNotification->sendEmailOTP($params);
+        }*/
+
+        $MobileNumber = $_POST['MobileNumber'];
+
+        $sql2 = "UPDATE registeredusers SET SingleUsePassword='$singleusepassword', SingleUseExpiry= '$timestamp' WHERE Trim(MobileNumber) = Trim('$MobileNumber')";
+        $stmt2 = $con->prepare($sql2);
+        $res2 = $stmt2->execute();
+        echo json_encode($usrRows);
+    } else {
+        echo "login error";
+    }
 }
 
 function sendSMS($nos, $message)
