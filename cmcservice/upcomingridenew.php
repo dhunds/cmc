6614e669aa4efa1d9e093982df77d21fd1f4cd9c
Mukul Kumar <mukul.kumar@clubmycab.com>
2016-02-11 19:36:56 +0530

    minor change

--- cmcservice/upcomingridenew.php
+++ cmcservice/upcomingridenew.php
@@ -11,40 +11,40 @@ if ($CabsExists > 0) {
         $FromShortAddress = $row['fromshortname'];
         $ToShortAddress = $row['toshortname'];
         $OwnerNumber = (string)$row['MobileNumber'];
-    }
 
-    $UpcomingTripNotification = "You have an upcoming trip from " . $FromShortAddress . " to " . $ToShortAddress . ". Click here to view details.";
 
-    $stmt1 = $con->query("select a.* from registeredusers a, acceptedrequest b where a.PushNotification != 'off' and Trim(a.MobileNumber) = Trim(b.MemberNumber) and b.cabid = '$CabID'");
-    $no_of_users = $con->query("SELECT FOUND_ROWS()")->fetchColumn();
+        $UpcomingTripNotification = "You have an upcoming trip from " . $FromShortAddress . " to " . $ToShortAddress . ". Click here to view details.";
 
-    if ($no_of_users > 0) {
-        while ($row = $stmt1->fetch()) {
-            $gcm_array = array();
-            $apns_array = array();
-            $FriendPlatform = $row['Platform'];
-            $MemberName = $row['FullName'];
-            $MemberNumber = (string)$row['MobileNumber'];
+        $stmt1 = $con->query("select a.* from registeredusers a, acceptedrequest b where a.PushNotification != 'off' and Trim(a.MobileNumber) = Trim(b.MemberNumber) and b.cabid = '$CabID'");
+        $no_of_users = $con->query("SELECT FOUND_ROWS()")->fetchColumn();
 
-            $body = array('gcmText' => $UpcomingTripNotification, 'pushfrom' => 'upcomingtrip', 'CabId' => $CabID);
+        if ($no_of_users > 0) {
+            while ($row1 = $stmt1->fetch()) {
+                $gcm_array = array();
+                $apns_array = array();
+                $FriendPlatform = $row1['Platform'];
+                $MemberName = $row1['FullName'];
+                $MemberNumber = (string)$row1['MobileNumber'];
 
-            if ($FriendPlatform == "A") {
-                $gcm_array[] = $row['DeviceToken'];
-                $objNotification->setVariables($gcm_array, $body);
-                $objNotification->sendGCMNotification();
-            } else {
-                $apns_array[] = $row['DeviceToken'];
-                $objNotification->setVariables($apns_array, $body);
-                $objNotification->sendIOSNotification();
+                $body = array('gcmText' => $UpcomingTripNotification, 'pushfrom' => 'upcomingtrip', 'CabId' => $CabID);
+
+                if ($FriendPlatform == "A") {
+                    $gcm_array[] = $row['DeviceToken'];
+                    $objNotification->setVariables($gcm_array, $body);
+                    $objNotification->sendGCMNotification();
+                } else {
+                    $apns_array[] = $row['DeviceToken'];
+                    $objNotification->setVariables($apns_array, $body);
+                    $objNotification->sendIOSNotification();
+                }
+
+                $NotificationType = "Upcoming Trip";
+                $cronsql = "INSERT INTO cronnotifications(NotificationType,SentMemberName, SentMemberNumber, ReceiveMemberName, ReceiveMemberNumber, Message, CabId, DateTime) VALUES ('$NotificationType','System','','$MemberName','$MemberNumber','$UpcomingTripNotification','$CabID',now())";
+                $cronstmt = $con->prepare($cronsql);
+                $cronres = $cronstmt->execute();
             }
-
-            $NotificationType = "Upcoming Trip";
-            $cronsql = "INSERT INTO cronnotifications(NotificationType,SentMemberName, SentMemberNumber, ReceiveMemberName, ReceiveMemberNumber, Message, CabId, DateTime) VALUES ('$NotificationType','System','','$MemberName','$MemberNumber','$UpcomingTripNotification','$CabID',now())";
-            $cronstmt = $con->prepare($cronsql);
-            $cronres = $cronstmt->execute();
         }
     }
-
     $sql12 = "UPDATE cabopen set uptripnotification = '1' where cabid = '$CabID'";
     $stmt12 = $con->prepare($sql12);
     $res12 = $stmt12->execute();
