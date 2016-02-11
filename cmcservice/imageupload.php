7157bacb6423ae3e1c51416c33d7b88390bb058c
Mukul Kumar <mukul.kumar@clubmycab.com>
2016-02-11 17:30:14 +0530

    image upload script changed

--- cmcservice/imageupload.php
+++ cmcservice/imageupload.php
@@ -1,39 +1,22 @@
 <?php
-
 include ('connection.php');
 
-	$MobileNumber = $_POST['MobileNumber'];
-	$ImageName = $MobileNumber . time() .'.jpg';
+$MobileNumber = $_POST['MobileNumber'];
+$imageName = $MobileNumber . '.png';
+$file = 'ProfileImages/'. $imageName;
 
-	$base=$_REQUEST['imagestr'];
-    $handle = fopen('test.txt', 'w');
 
-    $binary=base64_decode($base);
-    header('Content-Type: bitmap; charset=utf-8');
-	
-	if (!$file = fopen('/var/www/html/cmc/cmcservice/ProfileImages/' . $ImageName, 'wb')) {
-         echo "Error";
-         exit;
-    }
-	
-	if (fwrite($file, $binary) === FALSE) {
-        echo "Error";
-        exit;
-    }
-	
-    $file = fopen('ProfileImages/' . $ImageName, 'x');
-    fwrite($file, $binary);
-    fclose($file);
-	
-	$sql3 = "UPDATE `userprofileimage` SET `imagename`= '$ImageName' WHERE `MobileNumber` = '$MobileNumber'";
-	$stmt3 = $con->prepare($sql3);
-	$res3 = $stmt3->execute();
+$base=$_REQUEST['imagestr'];
+$base = str_replace(' ', '+', $base);
+$binary = base64_decode($base);
 
-	if ($res3 === true) 
-	{
-		 echo $ImageName;
-	}
-	else {
-		echo 'Error';
-	}
- ?>
+if (file_put_contents($file, $binary)){
+	$sql = "UPDATE userprofileimage SET imagename= '$ImageName' WHERE MobileNumber = '$MobileNumber'";
+	$stmt = $con->prepare($sql);
+	$stmt->execute();
+	echo $imageName;
+	exit;
+} else {
+	echo "Error";
+    exit;
+}
