9f9bc34d619cffd1c69fb3a1e0a99ced65725ec3
Mukul Kumar <mukul.kumar@clubmycab.com>
2016-04-20 12:27:36 +0530

    minor fix

--- cmcservice/getVehicle.php
+++ cmcservice/getVehicle.php
@@ -1,26 +1,16 @@
 <?php
-	include('connection.php');
+include('connection.php');
 
-if (isset($_POST['q']) && $_POST['q']) {
-	$keyword = $_POST['q'];
-	$sql = "SELECT * FROM vehicle WHERE vehicleModel LIKE '%$keyword%'";
+$keyword = $_POST['q'];
+$sql = "SELECT * FROM vehicle WHERE vehicleModel LIKE '%$keyword%'";
 
-	$stmt = $con->query($sql);
-	$vehicles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$stmt = $con->query($sql);
+$vehicles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-	$response['status'] = 'success';
-	$response['data'] = $vehicles;
+$response['status'] = 'success';
+$response['data'] = $vehicles;
 
-	http_response_code(200);
-    header('Content-Type: application/json');
-    echo json_encode($response);
-    exit;
-
-} else {
-	http_response_code(500);
-    header('Content-Type: application/json');
-    echo '{"status":"failed", "message":"Invalid Params."}';
-    exit;
-}
-
-
+http_response_code(200);
+header('Content-Type: application/json');
+echo json_encode($response);
+exit;
