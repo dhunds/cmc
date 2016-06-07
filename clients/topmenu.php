8b282e7c64744bd5f181ad745dfbfefd653517a4
Mukul Kumar <mukul.kumar@clubmycab.com>
2016-06-07 13:34:37 +0530

    add type for different clients

--- clients/topmenu.php
+++ clients/topmenu.php
@@ -1,7 +1,7 @@
 <div id="demo-horizontal-menu">
     <ul id="std-menu-items">
 <!--        <li class="pure-menu-selected"><a href="dashboard.php"><i class="fa fa-home"></i>&nbsp;Home</a></li>-->
-        <?php if ($_SESSION['username']=='cabservice') { ?>
+        <?php if ($_SESSION['type']==2) { ?>
              <li class="pure-menu-selected"><a href="createRide.php">Create Ride</a></li>
              <li class="pure-menu-selected"><a href="changepswd.php">Change Password</a></li>
              <li class="pure-menu-selected" style="float: right;"><a href="logout.php">Logout</a></li>
@@ -12,7 +12,7 @@
         <?php } ?>
     </ul>
 </div>
-<?php if ($_SESSION['username'] !='cabservice') { ?>
+<?php if ($_SESSION['type'] !=2) { ?>
 <div style="text-align: right;float: right;margin:10px;">
     <form method="post" action="search.php">
         <input type="text" name="keyword" placeholder="search members.." value="<?=(isset($_POST['keyword']))?$_POST['keyword']:'';?>">
