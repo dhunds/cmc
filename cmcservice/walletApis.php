27aa98c014b63b18f08f5ed05af92fe698e491b8
Mukul Kumar <mukul.kumar@clubmycab.com>
2016-07-07 12:08:23 +0530

    checkbalanceforride added for cash

--- cmcservice/walletApis.php
+++ cmcservice/walletApis.php
@@ -8,52 +8,14 @@ include('includes/functions.php');
 
 if (isset($_POST['act']) && $_POST['act'] !='' && isset($_POST['mobileNumber']) && $_POST['mobileNumber'] !='' && isset($_POST['paymentMethod']) && $_POST['paymentMethod'] !='') {
     $paymentMethod = $_POST['paymentMethod'];
-    $objWallet = new $paymentMethod();
     $mobileNumber = $_POST['mobileNumber'];
 
-    if ($_POST['act'] == 'saveToken') {
-
-        $resp = $objWallet->saveToken($_POST['mobileNumber'], $_POST['token']);
-
-        if ($resp) {
-            setResponse(array("code"=>200, "status"=>"success", "message"=>"Token Saved"));
-
-        } else {
-            setResponse(array("code"=>200, "status"=>"fail", "message"=>"An error occured, please try again."));
-        }
-
-    } else if($_POST['act'] == 'checkBalance') {
-
-        $resp = $objWallet->checkBalance($_POST['mobileNumber']);
-
-        if ($resp && $resp->status =='SUCCESS') {
-            setResponse(array("code"=>200, "status"=>"success", "balance"=>(string)$resp->balanceamount));
-        } else {
-            setResponse(array("code"=>200, "status"=>"fail", "statuscode"=>(string)$resp->statuscode, 'statusdescription'=>(string)$resp->statusdescription));
-        }
-
-    } else if($_POST['act'] == 'checkBalanceForRide') {
-
+    if ($paymentMethod=='Cash' && $_POST['act'] == 'checkBalanceForRide'){
+        
         $user = getUserByMobileNumber($_POST['mobileNumber']);
-        $mobikwikBalance = 0;
-
-        // Check If wallet is linked
-        $resp = $objWallet->getWallet($_POST['mobileNumber']);
-
-        if (empty($resp) || $resp['token'] =='') {
-            setResponse(array("code"=>200, "status"=>"success", "walletStatusCode"=>"1", "message"=>"Wallet not linked"));
-        }
-
-        // Proceed to balance check if wallet linked
-        $resp = $objWallet->checkBalance($_POST['mobileNumber']);
-
-        if ($resp && $resp->status =='SUCCESS') {
-            $mobikwikBalance = $resp->balanceamount;
-        }
-
-        $discount = 0;
         $credit = $user ['totalCredits'];
 
+        $discount = 0;
         $offerDetail = checkForOffers ($mobileNumber);
 
         if ($offerDetail) {
@@ -62,13 +24,12 @@ if (isset($_POST['act']) && $_POST['act'] !='' && isset($_POST['mobileNumber']) 
             $discount = 0;
         }
 
-        // Check User Balance (including discounts and credits) for the ride
-        $availableBalance = $credit + $discount + $mobikwikBalance;
+        $availableBalance = $credit + $discount;
         $requiredBalance = $_POST['amount'];
 
         if ($requiredBalance > $availableBalance){
 
-            $jsonResp = array("code"=>200, "status" => "success", "walletStatusCode"=>"2", "balance" => ($resp->balanceamount + $credit + $discount), "message" =>"Insufficient balance for ride");
+            $jsonResp = array("code"=>200, "status" => "success", "walletStatusCode"=>"2", "balance" => $availableBalance, "message" =>"Insufficient balance for ride");
         } else {
 
             // Amount payable by rider
@@ -82,20 +43,100 @@ if (isset($_POST['act']) && $_POST['act'] !='' && isset($_POST['mobileNumber']) 
                 }
             }
 
-            $jsonResp = array("code"=>200, "status" => "success", "walletStatusCode"=>"3", "balance" => ($resp->balanceamount + $credit + $discount), "payableByRider"=>$payableByRider, "message" =>"Balance sufficient for ride");
+            $jsonResp = array("code"=>200, "status" => "success", "walletStatusCode"=>"3", "balance" => $availableBalance, "payableByRider"=>$payableByRider, "message" =>"Balance sufficient for ride");
         }
 
         setResponse($jsonResp);
+    } else {
 
-    } else if($_POST['act'] == 'getToken') {
+        $objWallet = new $paymentMethod();
 
-        $resp = $objWallet->getWallet($_POST['mobileNumber']);
+        if ($_POST['act'] == 'saveToken') {
 
-        if (!empty($resp)) {
-            setResponse(array("code"=>200, "status"=>"success", "token"=>$resp['token']));
+            $resp = $objWallet->saveToken($_POST['mobileNumber'], $_POST['token']);
 
-        } else {
-            setResponse(array("code"=>200, "status"=>"fail", "message"=>"No Data"));
+            if ($resp) {
+                setResponse(array("code"=>200, "status"=>"success", "message"=>"Token Saved"));
+
+            } else {
+                setResponse(array("code"=>200, "status"=>"fail", "message"=>"An error occured, please try again."));
+            }
+
+        } else if($_POST['act'] == 'checkBalance') {
+
+            $resp = $objWallet->checkBalance($_POST['mobileNumber']);
+
+            if ($resp && $resp->status =='SUCCESS') {
+                setResponse(array("code"=>200, "status"=>"success", "balance"=>(string)$resp->balanceamount));
+            } else {
+                setResponse(array("code"=>200, "status"=>"fail", "statuscode"=>(string)$resp->statuscode, 'statusdescription'=>(string)$resp->statusdescription));
+            }
+
+        } else if($_POST['act'] == 'checkBalanceForRide') {
+
+            $user = getUserByMobileNumber($_POST['mobileNumber']);
+            $mobikwikBalance = 0;
+
+            // Check If wallet is linked
+            $resp = $objWallet->getWallet($_POST['mobileNumber']);
+
+            if (empty($resp) || $resp['token'] =='') {
+                setResponse(array("code"=>200, "status"=>"success", "walletStatusCode"=>"1", "message"=>"Wallet not linked"));
+            }
+
+            // Proceed to balance check if wallet linked
+            $resp = $objWallet->checkBalance($_POST['mobileNumber']);
+
+            if ($resp && $resp->status =='SUCCESS') {
+                $mobikwikBalance = $resp->balanceamount;
+            }
+
+            $discount = 0;
+            $credit = $user ['totalCredits'];
+
+            $offerDetail = checkForOffers ($mobileNumber);
+
+            if ($offerDetail) {
+                $discount = $offerDetail['amount'];
+            } else {
+                $discount = 0;
+            }
+
+            // Check User Balance (including discounts and credits) for the ride
+            $availableBalance = $credit + $discount + $mobikwikBalance;
+            $requiredBalance = $_POST['amount'];
+
+            if ($requiredBalance > $availableBalance){
+
+                $jsonResp = array("code"=>200, "status" => "success", "walletStatusCode"=>"2", "balance" => ($resp->balanceamount + $credit + $discount), "message" =>"Insufficient balance for ride");
+            } else {
+
+                // Amount payable by rider
+                if (($discount + $credit) < 1) {
+                    $payableByRider = $requiredBalance;
+                } else {
+                    if ($requiredBalance <= $discount || ($requiredBalance <= ($discount + $credit))) {
+                        $payableByRider = 0;
+                    } else {
+                        $payableByRider = $requiredBalance - ($discount + $credit);
+                    }
+                }
+
+                $jsonResp = array("code"=>200, "status" => "success", "walletStatusCode"=>"3", "balance" => ($resp->balanceamount + $credit + $discount), "payableByRider"=>$payableByRider, "message" =>"Balance sufficient for ride");
+            }
+
+            setResponse($jsonResp);
+
+        } else if($_POST['act'] == 'getToken') {
+
+            $resp = $objWallet->getWallet($_POST['mobileNumber']);
+
+            if (!empty($resp)) {
+                setResponse(array("code"=>200, "status"=>"success", "token"=>$resp['token']));
+
+            } else {
+                setResponse(array("code"=>200, "status"=>"fail", "message"=>"No Data"));
+            }
         }
     }
 } else {
