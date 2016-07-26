bffe60ae7d2b9d56a7d12b2ebd93312fa0cd5916
Mukul Kumar <mukul.kumar@clubmycab.com>
2016-07-26 13:57:40 +0530

    minor fixes

--- cmcservice/ridesAvailable.php
+++ cmcservice/ridesAvailable.php
@@ -250,10 +250,10 @@ if (isset($_POST['sLatLon']) && isset($_POST['mobileNumber']) && $_POST['mobileN
     $condition = '';
 
     if (!empty($groupCities)) {
-        $groupCitiesStr = implode(',', $groupCities);
+        $groupCitiesStr = implode("','", $groupCities);
         $condition = ' AND co.toCity NOT IN ('.$groupCitiesStr.') ';
     } else {
-        $condition = ' AND co.toCity NOT IN ('.$fromCity.') ';
+        $condition = " AND co.toCity NOT IN ('".$fromCity."') ";
     }
 
     $sql = "SELECT  co.CabId, co.MobileNumber, co.OwnerName, co.FromLocation, co.ToLocation, co.FromShortName, co.ToShortName, co.fromCity, co.toCity, co.sLatLon, co.eLatLon, co.TravelDate, co.TravelTime, co.Seats, co.Distance, co.ExpTripDuration, co.OpenTime, co.CabStatus, co.status, co.RateNotificationSend, co.ExpStartDateTime, co.ExpEndDateTime, co.OwnerChatStatus, co.FareDetails, co.RemainingSeats, 'N' As IsOwner, CONCAT((co.Seats - co.RemainingSeats),'/', co.Seats) as Seat_Status, co.rideType, co.perKmCharge, ui.imagename, cr.BookingRefNo, cn.CabName, cr.DriverName, cr.DriverNumber, cr.CarNumber, cr.CarType, pm.PoolId, pm.PoolName, pm.rGid, v.vehicleModel, vd.registrationNumber, vd.isCommercial, ru.socialType, ru.CreatedOn
