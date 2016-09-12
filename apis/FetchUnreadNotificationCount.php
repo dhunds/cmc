affbbe298a3af49c0e91d8e359e0f0c2b2ede72e
Mukul Kumar <mukul.kumar@clubmycab.com>
2016-09-12 17:28:02 +0530

    minor change

--- apis/FetchUnreadNotificationCount.php
+++ apis/FetchUnreadNotificationCount.php
@@ -2,9 +2,10 @@
 
  include ('connection.php');
 	
+$mobileNumber = $_POST['MobileNumber'];
 $userId = $_POST['userId'];
 
-$sql = "SELECT count(*) FROM notifications WHERE Trim(receivedMemberUserId) = Trim('$userId') AND Trim(Status) = 'U' AND Trim(StatusArchieve) = 'No' and RefStatus IS NULL";
+$sql = "SELECT count(*) FROM notifications WHERE Trim(ReceiveMemberNumber) = Trim('$mobileNumber') AND Trim(Status) = 'U' AND Trim(StatusArchieve) = 'No' and RefStatus IS NULL";
 $result = $con->prepare($sql); 
 $result->execute(); 
 $number_of_rows = $result->fetchColumn(); 
