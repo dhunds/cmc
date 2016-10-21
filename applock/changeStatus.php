54e462fd7b8396bc93e94c22a227d716d257eed1
Mukul Kumar <mukul.kumar@clubmycab.com>
2016-10-21 13:07:02 +0530

    added nextCheckDateTime

--- applock/changeStatus.php
+++ applock/changeStatus.php
@@ -7,12 +7,14 @@ if (isset($_POST['imei']) && $_POST['imei'] != '' && isset($_POST['status']) && 
 
     $imei = (isset($_POST['imei']))?$_POST['imei']:'';
     $status = (isset($_POST['status']))?$_POST['status']:'';
+    $nextCheckDateTime = $_POST['nextCheckDateTime'];
 
     $stmt = $con->query("SELECT *  FROM deviceDetails WHERE IMEI='".$_POST['imei']."'");
     $found = $con->query("SELECT FOUND_ROWS()")->fetchColumn();
 
     if ($found) {
-        $sql = "UPDATE deviceDetails SET status=$status WHERE imei='$imei'";
+
+        $sql = "UPDATE deviceDetails SET status=$status, nextCheckDateTime='$nextCheckDateTime' WHERE imei='$imei'";
         $stmt = $con->prepare($sql);
         $stmt->execute();
         $resp = array('code' => 200, 'status' => 'success');
